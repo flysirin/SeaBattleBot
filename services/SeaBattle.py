785643f13eb10213cd5b8d7b587ed96071f7ee8b
Stanislaus <flysirin@gmail.com>
2024-06-06 00:29:39 +0400

    create damege_register and miss_register functions

--- services/SeaBattle.py
+++ services/SeaBattle.py
@@ -7,8 +7,8 @@ class Ship:
         self._tp = tp
         self._length = length  # number of decks
         self._is_move = True
-        self._cells = cells if cells else [1 for _ in range(length)]  # condition of ship: 1 or 2 - damage
-        self._ship_cells, self._around_ship = set(), set()
+        self.cells_state = cells if cells else [1 for _ in range(length)]  # condition of ship: 1 or 2 - damage
+        self.ship_cells, self._around_ship = set(), set()
         self._size_field = size_field
         if type(x) is int and type(y) is int:
             self.cells_ship_id()
@@ -23,7 +23,7 @@ class Ship:
         return self._x, self._y
 
     def move(self, go: int):  # move ship, example: -1 or 1
-        if any(x == 2 for x in self._cells):
+        if any(x == 2 for x in self.cells_state):
             self._is_move = False
             return False
         self._x = self._x + go * self._tp
@@ -33,8 +33,8 @@ class Ship:
         return True
 
     def cells_ship_id(self):  # set cells ship id
-        self._ship_cells = {(self._y + i * (self._tp ^ 1)) * self._size_field + self._x + i * self._tp + 1 for i in
-                            range(self._length)}
+        self.ship_cells = {(self._y + i * (self._tp ^ 1)) * self._size_field + self._x + i * self._tp + 1 for i in
+                           range(self._length)}
 
     def cells_around_ship_id(self):  # set cells surrounding the ship id
         self._around_ship = set()
@@ -47,8 +47,8 @@ class Ship:
                                 self._x + shift * self._tp + i) + 1)
 
     def is_collide(self, ship: 'Ship'):  # -> True, if there is a collision with another ship
-        if self._ship_cells and self._around_ship and ship._around_ship and ship._ship_cells:
-            return not self._around_ship.isdisjoint(ship._ship_cells)
+        if self.ship_cells and self._around_ship and ship._around_ship and ship.ship_cells:
+            return not self._around_ship.isdisjoint(ship.ship_cells)
 
     def is_out_pole(self, size):  # -> True, if there is going beyond borders
         size = self._size_field if not size else size
@@ -58,12 +58,12 @@ class Ship:
         return False
 
     def __getitem__(self, index):  # get condition ship cell
-        return self._cells[index]
+        return self.cells_state[index]
 
     def __setitem__(self, index, value):  # set condition ship cell
         if value != 2:
             raise ValueError("Expected type int = 2")
-        self._cells[index] = value
+        self.cells_state[index] = value
         self._is_move = False
 
     def __repr__(self):
@@ -75,7 +75,8 @@ class Game:
         self._size = size
         self._c_size = size - 1  # coordinates size
         self._ships = ships if ships else []
-        self._pole = [[0 for _ in range(size)] for _ in range(size)]
+        self._field = [[0 for _ in range(size)] for _ in range(size)]
+        self._miss_field = [[0 for _ in range(size)] for _ in range(size)]
 
     def size(self):
         return self._size
@@ -104,27 +105,26 @@ class Game:
 
         while len(self._ships) != 10:
             lineup_ships()
-        self._upd_pole()
+        self._upd_field()
 
     def get_ships(self):
         return self._ships
 
-    def _upd_pole(self):
-        self._pole = [[0 for _ in range(self._size)] for _ in range(self._size)]
+    def _upd_field(self):
+        self._field = [[0 for _ in range(self._size)] for _ in range(self._size)]
         for ship in self._ships:
             x_0, y_0 = ship.get_start_coords()
             turn, not_turn = (ship._tp ^ 1), ship._tp
             c = 0
             for cell_ship in ship:
-                self._pole[y_0 + c * turn][x_0 + c * not_turn] = cell_ship
+                self._field[y_0 + c * turn][x_0 + c * not_turn] = cell_ship
                 c += 1
 
-    def get_pole(self):
-        return tuple(tuple(x) for x in self._pole)
+    def get_field(self):
+        return tuple(tuple(x) for x in self._field)
 
-    def show(self):
-        for line in self._pole:
-            print(*line)
+    def get_miss_field(self):
+        return tuple(tuple(x) for x in self._miss_field)
 
     def _can_move(self, ship: Ship, distance: int):
         if not ship._is_move:
@@ -144,8 +144,27 @@ class Game:
                 ship.move(go_trip)
             elif self._can_move(ship, -go_trip):
                 ship.move(-go_trip)
-        self._upd_pole()
+        self._upd_field()
 
+    def get_ship_by_cell_id(self, cell_id: int):
+        if self._ships:
+            for ship in self._ships:
+                if cell_id in ship.ship_cells:
+                    return ship
+
+    def damage_register(self, x: int, y: int):
+        """x,y ∈ [1-field_size]"""
+        if not (1 <= x <= self._size and 1 <= y <= self._size):
+            return
+        cell_id = (y - 1) * self._size + x
+        ship = self.get_ship_by_cell_id(cell_id)
+        if ship:
+            ship._is_move = False
+            num_cell_id = sorted(ship.ship_cells).index(cell_id)
+            ship.cells_state[num_cell_id] = 2
+            self._upd_field()
+        else:
+            self._miss_field[y - 1][x - 1] = 3
 
 # pole = Game(10)
 # pole.init()
